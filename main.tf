c4a12b257b7b5da256beea3133c101661490d6c8
zzkhangg <khang.vuong@student.uq.edu.au>
2026-03-28 16:29:57 +1000

    Implement hosting images in ECR.

--- main.tf
+++ main.tf
@@ -4,6 +4,11 @@ terraform {
         source = "hashicorp/aws"
         version = "~> 6.0"
     }
+
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "3.6.2"
+    }
   }
 }
 
@@ -12,8 +17,34 @@ provider "aws" {
   shared_config_files = ["./credentials"]
 }
 
+data "aws_ecr_authorization_token" "ecr_token" {}
+
+provider "docker" {
+  registry_auth {
+    address = data.aws_ecr_authorization_token.ecr_token.proxy_endpoint
+    username = data.aws_ecr_authorization_token.ecr_token.user_name
+    password = data.aws_ecr_authorization_token.ecr_token.password
+  }
+}
+
+resource "aws_ecr_repository" "taskoverflow" {
+  name = "taskoverflow"
+}
+
+resource "docker_image" "taskoverflow" {
+  name = "${aws_ecr_repository.taskoverflow.repository_url}:latest"
+  build {
+    context = "."
+  }
+}
+
+resource "docker_registry_image" "taskoverflow" {
+  name = docker_image.taskoverflow.name
+}
+
+
 locals {
-  image = "ghcr.io/csse6400/taskoverflow:latest"
+  image = aws_ecr_repository.taskoverflow.repository_url
   database_username = "administrator"
   database_password = "vuongkhang2104"
 }
